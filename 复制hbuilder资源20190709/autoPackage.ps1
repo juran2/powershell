48c2c461110710493553e32d79e4c76106feb852
juran2 <46011938+juran2@users.noreply.github.com>
2019-07-09 13:19:19 +0800

    Add files via upload

---
+++ 复制hbuilder资源20190709/autoPackage.ps1
@@ -0,0 +1,42 @@
+﻿#未完成
+
+#定义一些路径
+$used_path="C:\Users\user\Desktop\$args[0]"
+$android_path="C:\Users\user\Desktop\$args[1]"
+$hbuilder_path="D:\APP模板及项目\APP项目\$args[2]"
+
+$old_id="com.ouryoo.app.game"
+$new_id="net.bzgame.app.jinxing"
+
+#把xx.xx.xx.xx替换为xx\xx\xx\xx
+$told_id=$old_id.Replace('.','\')
+$tnew_id=$new_id.Replace('.','\')
+
+#列举出要操作的文件
+$id_files="$android_path\app\build.gradle",
+          "$android_path\app\src\main\AndroidManifest.xml",
+          "$android_path\app\src\main\assets\data\dcloud_control.xml",
+          "$android_path\app\src\main\java\*\*\*\*\wxapi\WXEntryActivity.java",
+          "$android_path\app\src\main\java\*\*\*\*\wxapi\WXPayEntryActivity.java"
+$name_files="$android_path\app\build.gradle",
+            "$android_path\app\src\mian\res\values\strings.xml"
+
+#替换文件中的appid
+foreach($file in $id_files)
+{
+    (Get-Content -Encoding "UTF8" $file) | ForEach-Object{ $_ -replace $args[0],$args[1] } | Set-Content $file -Encoding "UTF8"
+     Write-Host -ForegroundColor Black $file"已经完成替换"
+}
+
+$wxpath="$android_path\app\src\main\java"
+#替换微信登陆目录结构
+if(Test-Path $wxpath)
+{
+    #mkdir "$wxpath\net\bzgame\app\star\"
+    Rename-Item -Recurse "$wxpath\*\*\*\*\" "$wxpath\net\bzgame\app\jinxing\"
+    Write-Host -ForegroundColor Green "新建成功"
+}
+
+    
+
+
---
+++ 复制hbuilder资源20190709/update_to_android_studio.ps1
@@ -0,0 +1,59 @@
+﻿#操作路径
+$packageDir='C:\Users\admin\Desktop'
+$gameDir='D:\APP模板及项目\APP项目'
+
+$optgame=''
+$optpackage=''
+
+
+#检验项目是否存在
+$flag1=$false
+$games=Get-ChildItem $gameDir
+foreach($game in $games){
+    if($game.Name -eq $args[0]){      
+        $flag1=$true
+        $optgame=$game
+    }
+}
+if($flag1 -eq $false){
+    Write-Host -ForegroundColor Red $args[0]':游戏没有找到!'
+    return
+}
+else{
+     Write-Host -ForegroundColor Green $optgame.Name':找到游戏'
+}
+
+#检验打包机是否存在
+$flag2=$false
+$packages=Get-ChildItem $packageDir
+foreach($package in $packages){
+    if(('.\'+$package.Name  -eq $args[1]) -or ($package.Name -eq $args[1])){      
+        $flag2=$true
+        $optpackage=$package
+    }
+}
+if($flag2 -eq $false){
+    Write-Host -ForegroundColor Red $args[1]':打包机没有找到!'
+    return
+}
+else{
+     Write-Host -ForegroundColor Green $optpackage.Name':找到打包机'
+}
+
+#确定包名
+$appid=''
+$v=Get-ChildItem $packageDir\$optpackage\app\src\main\assets\apps
+
+foreach($a in $v){
+   
+    if(-not ($a.Name -eq '.DS_Store'))
+    {
+        $appid=$a
+    }
+}
+Write-Host -ForegroundColor Green 'appid:'$appid
+Write-Host -ForegroundColor Yellow '正在更新...'
+
+#项目更新
+Copy-Item -Recurse -Force $gameDir\$optgame\*  -Destination  $packageDir\$optpackage\app\src\main\assets\apps\$appid\www
+Write-Host -ForegroundColor Green $optpackage.Name'更新成功!'
